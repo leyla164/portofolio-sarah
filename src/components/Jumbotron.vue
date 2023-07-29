e47ac29cbd6d22ad3767ac3b83737dd7eb9f963f
leyla164 <leylamaisarahdasni@gmail.com>
2023-07-29 13:48:39 +0700

    portofoli-sarah

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -3,7 +3,7 @@
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
         <div class="flex flex-col justify-center">
             <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Hallo!!</h1>
-            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Assalamualaikum Warahmatullah Wabarakatul,Saya akan memperkenalkan secara singkat.Perkenalkan nama saya leyla maisarah dasni,Dipanggil sarah 
+            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Assalamualaikum Warahmatullah Wabarakatu,Saya akan memperkenalkan secara singkat.Perkenalkan nama saya leyla maisarah dasni,Dipanggil sarah 
                 umur saya 17 tahun saya bersekolah di SMK ASSALAAM BANDUNG.</p>
         </div>
         <div class="grid gap-4">
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@ import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from 'flowbite-vue'
     <Navbar solid>
       <template #logo>
         <NavbarLogo link="https://www.google.com/" alt="Flowbite logo" image-url="https://st3.depositphotos.com/6697918/15699/v/450/depositphotos_156995416-stock-illustration-beautiful-face-of-arabic-muslim.jpg">
-           <h2>PORTFOLIO</h2>
+           <h2>PORTOFOLIO</h2>
         </NavbarLogo>
       </template>
       <template #default="{isShowMenu}">
